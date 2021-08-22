57334d4399064e506caf46942995e014399f8db4
juli <oreshek41@yandex.ru>
2021-08-22 23:08:00 +0300

    HW9

--- hw9/main.py
+++ hw9/main.py
@@ -10,17 +10,17 @@ problem40 - list comprehension
 '''
 
 
-# #problem9
-# print("problem9")
-# [[[print(a*b*c) for c in range(b+1, 1000) if a+b+c == 1000 and a**2+b**2 == c**2] for b in range(a+1, 1000)] for a in range(0, 1000)]
-#
-# #problem6
-# print("problem6")
-# print((sum([c for c in range(1, 101)]))**2 - sum([c**2 for c in range(1, 101)]))
-#
-# #problem48
-# print("problem48")
-# print(str(sum([c**c for c in range(1, 1000)]))[::-1][0:10][::-1])
+#problem9
+print("problem9")
+[[[print(a*b*c) for c in range(b+1, 1000) if a+b+c == 1000 and a**2+b**2 == c**2] for b in range(a+1, 1000)] for a in range(0, 1000)]
+
+#problem6
+print("problem6")
+print((sum([c for c in range(1, 101)]))**2 - sum([c**2 for c in range(1, 101)]))
+
+#problem48
+print("problem48")
+print(str(sum([c**c for c in range(1, 1000)]))[::-1][0:10][::-1])
 
 #problem40
 print("problem40")
