855847c43a87e9eddfe30a20141e25e134ea063b
juli <oreshek41@yandex.ru>
2021-09-16 21:35:20 +0300

    HW21

--- hw21/main.py
+++ hw21/main.py
@@ -7,13 +7,28 @@ list(merge((x for x in range(1,4)),(x for x in range(2,5)))) == [1,2,2,3,3,4]
 
 
 def merge(a, b):
-    full = []
-    for i in a:
-        full.append(i)
-    for i in b:
-        full.append(i)
-    full.sort()
-    return full
+    lst = []
+    while True:
+        try:
+            itr = next(a)
+        except StopIteration:
+            break
+        else:
+            lst.append(itr)
+    while True:
+        try:
+            itr = next(b)
+        except StopIteration:
+            break
+        else:
+            lst.append(itr)
+    for i in range(len(lst) - 1):
+        for j in range(len(lst) - i - 1):
+            if lst[j] > lst[j + 1]:
+                tmp = lst[j]
+                lst[j] = lst[j + 1]
+                lst[j+1] = tmp
+    return lst
 
 
 print(list(merge((x for x in range(1, 4)), (x for x in range(2, 5)))) == [1, 2, 2, 3, 3, 4])
